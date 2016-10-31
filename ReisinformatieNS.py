8c7474e12a19245fd87aaf3ec39bbd428d366756
MaartenMol96 <maartenmol96@gmail.com>
2016-10-31 11:03:23 +0100

    Start

--- ReisinformatieNS.py
+++ ReisinformatieNS.py
@@ -1,4 +1,5 @@
 #ReisinformatieNS main application
+#Gemaakt door Bastiaan Ebbenhorst, Joshua Offermans, Maarten Mol, Thomas Mocellin
 #Version 0.1
 
 import tkinter
@@ -6,6 +7,3 @@ top = tkinter.Tk()
 top.configure(background='#fcc917')
 top.geometry('{}x{}'.format(900,600))
 top.mainloop()
-
-
-#test change
