ac51720b99f9cf44227417f3071878e6c39de45e
MaartenMol96 <MaartenMol96@users.noreply.github.com>
2016-11-01 09:17:17 +0100

    Update ReisinformatieNS.py

--- ReisinformatieNS.py
+++ ReisinformatieNS.py
@@ -9,6 +9,11 @@ top = Tk()
 label = Label(master=top, text='Reisinformatie NS V0.1', background='yellow')
 label.pack()
 
+#App Buttons
+button = Button(master=top, text='Druk hier')
+button.pack(pady=10)
+
+
 top.configure(background='#fcc917')
 top.geometry('{}x{}'.format(900,600))
 top.mainloop()
