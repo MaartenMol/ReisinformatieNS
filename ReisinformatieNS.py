a93ec1fb069534f16a934d9cdc8907a358b3c7c9
Joshjess <joshuajessy47@gmail.com>
2016-11-01 10:49:56 +0100

    top

--- ReisinformatieNS.py
+++ ReisinformatieNS.py
@@ -10,7 +10,7 @@ from tkinter.messagebox import showinfo
 
 #Main App Settings
 app = Tk()
-app.configure(background='#fcc917')
+app.configure(background='#fece22')
 app.title("Reisinformatie NS V0.1")
 app.geometry('{}x{}'.format(790,600))
 app.resizable(width=False, height=False)
@@ -24,18 +24,24 @@ def NotInUse():
 #label = Label(master=app, text='Reisinformatie NS V0.1', background='yellow')
 #label.pack()
 
+#main img
+path1 = "beginscherm.jpg"
+img1 = ImageTk.PhotoImage(PIL.Image.open(path1))
+panel1 = Label(app, image = img1, borderwidth= 0)
+panel1.place(x=0, y=25)
+
 #Footer Image
-path = "balk.jpg"
-img = ImageTk.PhotoImage(PIL.Image.open(path))
-panel = Label(app, image = img)
-panel.place(x=0, y=549)
+path2 = "balk.jpg"
+img2 = ImageTk.PhotoImage(PIL.Image.open(path2))
+panel2 = Label(app, image = img2, borderwidth= 0)
+panel2.place(x=0, y=555)
 
 #App Buttons
-button0 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Ik wil reizen naar', command=NotInUse)
+button0 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Ik wil reizen naar', command=NotInUse)
 button0.place(x=100, y=400)
-button1 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Ik heb een OV-Chipkaart', command=NotInUse)
+button1 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Ik heb een OV-Chipkaart', command=NotInUse)
 button1.place(x=300, y=400)
-button2 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Reisinformatie')
+button2 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Reisinformatie')
 button2.place(x=600, y=400)
 
 #Show main App
