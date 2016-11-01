c1173b9909faf4d758e4f14439712e01a2f9c8c7
MaartenMol96 <maartenmol96@gmail.com>
2016-11-01 09:38:04 +0100

    Button

--- ReisinformatieNS.py
+++ ReisinformatieNS.py
@@ -10,7 +10,7 @@ label = Label(master=top, text='Reisinformatie NS V0.1', background='yellow')
 label.pack()
 
 #App Buttons
-button = Button(master=top, background='blue', text='Druk hier')
+button = Button(master=top, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Reisinformatie')
 button.pack(pady=10)
 
 
