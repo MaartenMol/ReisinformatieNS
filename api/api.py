e08469c823644b258adbd37c3321538344130a1a
MaartenMol96 <maartenmol96@gmail.com>
2016-11-01 11:45:22 +0100

    Order

--- api/api.py
+++ api/api.py
@@ -1,13 +1,9 @@
 import requests
 import xmltodict
 auth_details = ('joshuajessy47@gmail.com', 'vlgUm9-dkCiFX8swDoQ4uNdO1kiNtZhBs1CAIkbJl6gX3946BJ8uEQ')
-api_url = 'http://webservices.ns.nl/ns-api-avt?station=ut'
+Station = input(str('Naam van het station? '))
+api_url = 'http://webservices.ns.nl/ns-api-avt?station='+ Station
 response = requests.get(api_url, auth=auth_details)
-
-
-with open('vertrektijden.xml', 'w') as myXMLFile:
- myXMLFile.write(response.text)
-
 vertrekXML = xmltodict.parse(response.text)
 print('Dit zijn de vertrekkende treinen:')
 for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
