3c2bced9831922ca8b1f4f338f362defd1b591d6
MaartenMol96 <maartenmol96@gmail.com>
2016-11-01 11:03:25 +0100

    Structure en positions

--- ReisinformatieNS.py
+++ ReisinformatieNS.py
@@ -5,7 +5,7 @@
 #Import
 from PIL import ImageTk
 import PIL.Image
-from  tkinter import *
+from tkinter import *
 from tkinter.messagebox import showinfo
 
 #Main App Settings
@@ -21,28 +21,28 @@ def NotInUse():
 	showinfo(title='Melding Reisinformatie NS', message=bericht)
 
 #App Label
-#label = Label(master=app, text='Reisinformatie NS V0.1', background='yellow')
-#label.pack()
+label = Label(master=app, text='Reisinformatie NS BETA By Joshua & Maarten', background='yellow')
+label.pack()
 
-#main img
-path1 = "beginscherm.jpg"
-img1 = ImageTk.PhotoImage(PIL.Image.open(path1))
-panel1 = Label(app, image = img1, borderwidth= 0)
-panel1.place(x=0, y=25)
+#Main Image
+mainImagePath = "sources/main.jpg"
+mainImage = ImageTk.PhotoImage(PIL.Image.open(mainImagePath))
+mainImagePanel = Label(app, image = mainImage, borderwidth= 0)
+mainImagePanel.place(x=115, y=100)
 
 #Footer Image
-path2 = "balk.jpg"
-img2 = ImageTk.PhotoImage(PIL.Image.open(path2))
-panel2 = Label(app, image = img2, borderwidth= 0)
-panel2.place(x=0, y=555)
+footerImagePath = "sources/footer.jpg"
+footerImage = ImageTk.PhotoImage(PIL.Image.open(footerImagePath))
+footerImagePanel = Label(app, image = footerImage, borderwidth= 0)
+footerImagePanel.place(x=0, y=555)
 
 #App Buttons
 button0 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Ik wil reizen naar', command=NotInUse)
-button0.place(x=100, y=400)
+button0.place(x=50, y=425)
 button1 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Ik heb een OV-Chipkaart', command=NotInUse)
-button1.place(x=300, y=400)
+button1.place(x=275, y=425)
 button2 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Reisinformatie')
-button2.place(x=600, y=400)
+button2.place(x=575, y=425)
 
 #Show main App
 app.mainloop()
