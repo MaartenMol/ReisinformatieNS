a0561a9458fdf44203c505fa510f10dccf9ae11b
MaartenMol96 <MaartenMol96@users.noreply.github.com>
2016-11-01 09:15:54 +0100

    Update ReisinformatieNS.py

--- ReisinformatieNS.py
+++ ReisinformatieNS.py
@@ -4,6 +4,11 @@
 
 from tkinter import *
 top = Tk()
+
+#App Label
+label = Label(master=top, text='Reisinformatie NS V0.1', background='yellow')
+label.pack()
+
 top.configure(background='#fcc917')
 top.geometry('{}x{}'.format(900,600))
 top.mainloop()
