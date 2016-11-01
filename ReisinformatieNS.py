20cabad7d178fc5bed5fab59e1c0b4b34e34d709
MaartenMol96 <maartenmol96@gmail.com>
2016-11-01 19:38:07 +0100

    Reisinformatie menu toegevoegd en layout

--- ReisinformatieNS.py
+++ ReisinformatieNS.py
@@ -8,47 +8,44 @@ import PIL.Image
 from tkinter import *
 from tkinter.messagebox import showinfo
 
-
-
 #Main App Settings
 app = Tk()
 app.iconbitmap('sources/ns.ico')
 app.configure(background='#fece22')
-app.title("Reisinformatie NS V0.1")
+app.title("NS Kaartenautomaat")
 app.geometry('{}x{}'.format(790,600))
 app.resizable(width=False, height=False)
 
-#Reis Info Menu Settings
-#info = Tk()
-#info.iconbitmap('sources/ns.ico')
-#info.configure(background='#fece22')
-#info.title("Reisinformatie NS V0.1")
-#info.geometry('{}x{}'.format(790,600))
-#info.resizable(width=False, height=False)
+#Reis Info Menu
+def reisInfoMenu():
+    app.withdraw()
+    infoMenu = Tk()
+    infoMenu.iconbitmap('sources/ns.ico')
+    infoMenu.configure(background='#fece22')
+    infoMenu.title("Reisinformatie | NS Kaartenautomaat")
+    infoMenu.geometry('{}x{}'.format(790,600))
+    infoMenu.resizable(width=False, height=False)
 
-def quit():
-    app.destroy()
+    #infoMenu Label
+    label = Label(master=infoMenu, text='Reisinformatie NS BETA By Joshua & Maarten', background='yellow')
+    label.pack()
 
-#window2
-def win2():
-        app.withdraw()
-        new = Toplevel()
-        new.title('Window 2')
-        mainframe = Frame(new, padding = '3 3 12 12')
-        mainframe.grid(column = 0, row = 0, sticky = (N, W, E, S))
-        mainframe.columnconfigure(0, weight = 1)
-        mainframe.rowconfigure(0, weight = 1)
+    #infoMenu Buttons and Input
+    button3 = Button(master=infoMenu, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Reisinformatie ophalen', command=NotInUse)
+    button3.place(x=420, y=425)
+    entry1 = Entry(master=infoMenu, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', width=20,)
+    entry1.place(x=120, y=430)
 
-        Button(mainframe, text = 'Next', command = win2).grid(
-            column = 1, row = 1, sticky = W)
+    #Footer Image
+    footerInfoMenuImagePath = "sources/footerInfoMenu.jpg"
+    footerInfoMenuImage = ImageTk.PhotoImage(PIL.Image.open(footerInfoMenuImagePath))
+    footerInfoMenuImagePanel = Label(infoMenu, image = footerInfoMenuImage, borderwidth= 0)
+    footerInfoMenuImagePanel.place(x=0, y=555)
 
 #Functions
 def NotInUse():
-	bericht = 'Deze functie is niet beschikbaar in onze App!'
-	showinfo(title='Melding Reisinformatie NS', message=bericht)
-
-def ReisInfoMenu():
-	app.mainloop()
+    bericht = 'Deze functie is niet beschikbaar in onze App!'
+    showinfo(title='Melding Reisinformatie NS', message=bericht)
 
 #App Label
 label = Label(master=app, text='Reisinformatie NS BETA By Joshua & Maarten', background='yellow')
@@ -71,12 +68,8 @@ button0 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', 
 button0.place(x=50, y=425)
 button1 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Ik heb een OV-Chipkaart', command=NotInUse)
 button1.place(x=275, y=425)
-button2 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Reisinformatie', command=win2)
+button2 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Reisinformatie', command=reisInfoMenu)
 button2.place(x=575, y=425)
-
-
-
-
 
 #Show main App
 app.mainloop()
--- api/api.py
+++ api/api.py
@@ -1,13 +1,22 @@
+#Import
 import requests
 import xmltodict
+
+#API Authentication
 auth_details = ('joshuajessy47@gmail.com', 'vlgUm9-dkCiFX8swDoQ4uNdO1kiNtZhBs1CAIkbJl6gX3946BJ8uEQ')
+
+#User Input
 Station = input(str('Naam van het station? '))
+
+#API Query
 api_url = 'http://webservices.ns.nl/ns-api-avt?station='+ Station
 response = requests.get(api_url, auth=auth_details)
 vertrekXML = xmltodict.parse(response.text)
+
+#Result
 print('Dit zijn de vertrekkende treinen:')
 for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
- eindbestemming = vertrek['EindBestemming']
- vertrektijd = vertrek['VertrekTijd'] # 2016-09-27T18:36:00+0200
- vertrektijd = vertrektijd[11:16] # 18:36
- print('Om '+vertrektijd+' vertrekt een trein naar '+ eindbestemming)
+    eindbestemming = vertrek['EindBestemming']
+    vertrektijd = vertrek['VertrekTijd'] # 2016-09-27T18:36:00+0200
+    vertrektijd = vertrektijd[11:16] # 18:36
+    print('Om '+vertrektijd+' vertrekt een trein naar '+ eindbestemming)
