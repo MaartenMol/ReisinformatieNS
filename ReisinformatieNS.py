a0db96f0c0017ab14c8e895edfa7c85fd98ab588
MaartenMol96 <MaartenMol96@users.noreply.github.com>
2016-11-01 09:14:16 +0100

    Efficienter gemaakt
    from tkinter import *

--- ReisinformatieNS.py
+++ ReisinformatieNS.py
@@ -2,8 +2,8 @@
 #Gemaakt door Bastiaan Ebbenhorst, Joshua Offermans, Maarten Mol, Thomas Mocellin
 #Version 0.1
 
-import tkinter
-top = tkinter.Tk()
+from tkinter import *
+top = Tk()
 top.configure(background='#fcc917')
 top.geometry('{}x{}'.format(900,600))
 top.mainloop()
