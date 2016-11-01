b0a2fb2053bffbe113ab469332110c17d6cef32e
MaartenMol96 <maartenmol96@gmail.com>
2016-11-01 10:18:46 +0100

    Layout

--- ReisinformatieNS.py
+++ ReisinformatieNS.py
@@ -3,22 +3,27 @@
 #Version 0.1
 
 #Import
+from pil 
 from tkinter import *
 from tkinter.messagebox import showinfo
 
 
 
-#Main App
-top = Tk()
+#Main App Settings
+app = Tk()
+app.configure(background='#fcc917')
+app.title("Reisinformatie NS V0.1")
+app.geometry('{}x{}'.format(900,600))
 
-#Def
+
+#Functions
 def NotInUse():
 	bericht = 'Deze functie is niet beschikbaar in onze App!'
 	showinfo(title='Melding Reisinformatie NS', message=bericht)
 
 #App Label
-label = Label(master=top, text='Reisinformatie NS V0.1', background='yellow')
-label.pack()
+#label = Label(master=app, text='Reisinformatie NS V0.1', background='yellow')
+#label.pack()
 
 #layoutimg
 path = "balk.jpg"
@@ -28,15 +33,12 @@ panel.pack(side = "bottom", fill = "both", expand = "yes")
 
 
 #App Buttons
-button0 = Button(master=top, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Ik wil reizen naar', command=NotInUse)
+button0 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Ik wil reizen naar', command=NotInUse)
 button0.place(x=100, y=400)
-button1 = Button(master=top, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Ik heb een OV-Chipkaart', command=NotInUse)
+button1 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Ik heb een OV-Chipkaart', command=NotInUse)
 button1.place(x=300, y=400)
-button2 = Button(master=top, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Reisinformatie')
+button2 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Reisinformatie')
 button2.place(x=600, y=400)
 
-
-
-top.configure(background='#fcc917')
-top.geometry('{}x{}'.format(900,600))
-top.mainloop()
+#Show main App
+app.mainloop()
