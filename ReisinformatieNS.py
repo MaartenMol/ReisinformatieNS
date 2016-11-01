1d900926072f884a7b92473794f55eed788b4ede
MaartenMol96 <maartenmol96@gmail.com>
2016-11-01 10:00:41 +0100

    Buttons

--- ReisinformatieNS.py
+++ ReisinformatieNS.py
@@ -2,16 +2,30 @@
 #Gemaakt door Bastiaan Ebbenhorst, Joshua Offermans, Maarten Mol, Thomas Mocellin
 #Version 0.1
 
+#Import
 from tkinter import *
+from tkinter.messagebox import showinfo
+
+#Main App
 top = Tk()
+
+#Def
+def NotInUse():
+	bericht = 'Deze functie is niet beschikbaar in onze App!'
+	showinfo(title='Melding Reisinformatie NS', message=bericht)
 
 #App Label
 label = Label(master=top, text='Reisinformatie NS V0.1', background='yellow')
 label.pack()
 
 #App Buttons
-button = Button(master=top, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Reisinformatie')
-button.pack(pady=10)
+button0 = Button(master=top, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Ik wil reizen naar', command=NotInUse)
+button0.place(x=100, y=400)
+button1 = Button(master=top, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Ik heb een OV-Chipkaart', command=NotInUse)
+button1.place(x=300, y=400)
+button2 = Button(master=top, font=('Frutiger', 16, 'bold'), foreground='white', background='blue', text='Reisinformatie')
+button2.place(x=600, y=400)
+
 
 
 top.configure(background='#fcc917')
