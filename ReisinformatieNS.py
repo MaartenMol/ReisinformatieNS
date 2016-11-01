386f566fa9bfd899f9fd1cff2c863ac73c84e25d
Joshjess <joshuajessy47@gmail.com>
2016-11-01 23:26:57 +0100

    fixed

--- ReisinformatieNS.py
+++ ReisinformatieNS.py
@@ -8,44 +8,40 @@ import PIL.Image
 from tkinter import *
 from tkinter.messagebox import showinfo
 
-
-
 #Main App Settings
-def main(window):
-    window.iconbitmap('sources/ns.ico')
-    window.configure(background='#fece22')
-    window.title("Reisinformatie NS V0.1")
-    window.geometry('{}x{}'.format(790,600))
-    window.resizable(width=False, height=False)
-
-# app start
+def windowset(app):
+    app.iconbitmap('sources/ns.ico')
+    app.configure(background='#fece22')
+    app.geometry('{}x{}'.format(790,600))
+    app.resizable(width=False, height=False)
 app = Tk()
-main(app)
+app.title("NS Kaartenautomaat")
+windowset(app)
+#Reis Info Menu
+def reisInfoMenu():
+    infoMenu = Tk()
+    app.withdraw()
+    windowset(infoMenu)
+    #infoMenu Label
+    label = Label(master=infoMenu, text='Reisinformatie NS BETA By Joshua & Maarten', background='yellow')
+    label.pack()
 
-#Reis Info Menu Settings
-#info = Tk()
-#info.iconbitmap('sources/ns.ico')
-#info.configure(background='#fece22')
-#info.title("Reisinformatie NS V0.1")
-#info.geometry('{}x{}'.format(790,600))
-#info.resizable(width=False, height=False)
+    #infoMenu Buttons and Input
+    button3 = Button(master=infoMenu, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Reisinformatie ophalen', command=NotInUse)
+    button3.place(x=420, y=425)
+    entry1 = Entry(master=infoMenu, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', width=20,)
+    entry1.place(x=120, y=430)
 
-def quit():
-    app.destroy()
-
-#window2
-def win2():
-        app.withdraw()
-        new = Toplevel()
-        main(new)
+    #Footer Image
+    footerInfoMenuImagePath = "sources/footerInfoMenu.jpg"
+    footerInfoMenuImage = ImageTk.PhotoImage(PIL.Image.open(footerInfoMenuImagePath))
+    footerInfoMenuImagePanel = Label(infoMenu, image = footerInfoMenuImage, borderwidth= 0)
+    footerInfoMenuImagePanel.place(x=0, y=555)
 
 #Functions
 def NotInUse():
-	bericht = 'Deze functie is niet beschikbaar in onze App!'
-	showinfo(title='Melding Reisinformatie NS', message=bericht)
-
-def ReisInfoMenu():
-	app.mainloop()
+    bericht = 'Deze functie is niet beschikbaar in onze App!'
+    showinfo(title='Melding Reisinformatie NS', message=bericht)
 
 #App Label
 label = Label(master=app, text='Reisinformatie NS BETA By Joshua & Maarten', background='yellow')
@@ -68,12 +64,8 @@ button0 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', 
 button0.place(x=50, y=425)
 button1 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Ik heb een OV-Chipkaart', command=NotInUse)
 button1.place(x=275, y=425)
-button2 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Reisinformatie', command=win2)
+button2 = Button(master=app, font=('Frutiger', 16, 'bold'), foreground='white', background='#003399', text='Reisinformatie', command=reisInfoMenu)
 button2.place(x=575, y=425)
-
-
-
-
 
 #Show main App
 app.mainloop()
